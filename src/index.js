b63190101e5d6508ef7b4dc4924df9b063d5b2f7
bondaroff <st918284@dal.ca>
2022-06-02 15:51:38 -0700

    router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path='/' element={<App />}>
       </Route>
